f45cbeb87336dcad4efc5e9e7c618a05d8aa4ece
10647421 <10647421@INFPHSZA07218.lntinfotech.com>
2020-02-13 20:32:49 +0530

    updated with Mysql dependency in pom and mysqltest

--- src/test/java/com/imf/qa/test/imfHomePageTest_with_excel_testdata.java
+++ src/test/java/com/imf/qa/test/imfHomePageTest_with_excel_testdata.java
@@ -8,66 +8,50 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
 import com.imf.qa.base.BaseTest;
 import com.imf.qa.pages.imfHomePage;
 import com.imf.qa.util.Xls_Reader;
 
-
 public class imfHomePageTest_with_excel_testdata extends BaseTest {
-	
 
 	imfHomePage imfhomepage;
 	Xls_Reader reader;
- 
 
 	@BeforeClass
 	public void setup() throws Exception {
 
 		initExcelConnection();
 		initialization();
-		 
 
 	}
 
-	@Test(enabled = true, priority = 0 ,dataProvider= "ExcelTestData",description = "imf home page")
-	public void imfpagetitletest(Hashtable<String, String> mainTabs) throws Exception {
+	@Test(enabled = true, priority = 0, dataProvider = "ExcelTestData", description = "imf home page")
+	public void imfpagetitletest_excel(Hashtable<String, String> mainTabs) throws Exception {
 
-		
 		imfhomepage = new imfHomePage();
-		assertEquals(driver.getTitle(),mainTabs.get("TestData1"), "International Monetary Fund - Homepage");
-		assertEquals(imfhomepage.gettext_about(),mainTabs.get("TestData2"), "gettextabout");
+		assertEquals(driver.getTitle(), mainTabs.get("TestData1"), "International Monetary Fund - Homepage");
+		assertEquals(imfhomepage.gettext_about(), mainTabs.get("TestData2"), "gettextabout");
 		imfhomepage.click_country();
-		
-		List<String> li =imfhomepage.List_Countries_I();
-		assertEquals(li.get(0), mainTabs.get("TestData3") , "Iceland");
-		assertEquals(li.get(1),mainTabs.get("TestData4")  , "India");
-		assertEquals(li.get(2), mainTabs.get("TestData5") , "Indonesia");
-		assertEquals(li.get(3), mainTabs.get("TestData6") , "Iran, Islamic Republic of");
+
+		List<String> li = imfhomepage.List_Countries_I();
+		assertEquals(li.get(0), mainTabs.get("TestData3"), "Iceland");
+		assertEquals(li.get(1), mainTabs.get("TestData4"), "India");
+		assertEquals(li.get(2), mainTabs.get("TestData5"), "Indonesia");
+		assertEquals(li.get(3), mainTabs.get("TestData6"), "Iran, Islamic Republic of");
 	}
-	
 
 	@AfterClass
 	public void teardown() {
 
 		driver.close();
 	}
-	
-	 @DataProvider(name="ExcelTestData")
-     public Object[][] hashdata(Method m) {
 
+	@DataProvider(name = "ExcelTestData")
+	public Object[][] hashdata(Method m) {
 
-           return new Object[][]{  
-                 {readTestDataByMethodName(m.getName())}
-           };
+		return new Object[][] { { readTestDataByMethodName(m.getName()) } };
 
-     } 
-	
-	
-	
-	
-	
-	
+	}
+
 }
-
-	
-
