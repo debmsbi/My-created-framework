de1a334d2254782afc8b49846fd401e1144fc0f7
10647421 <10647421@INFPHSZA07218.lntinfotech.com>
2020-02-14 19:49:26 +0530

    updated redshiftdbtest.java file

--- src/test/java/com/imf/qa/test/redshiftdbtest.java
+++ src/test/java/com/imf/qa/test/redshiftdbtest.java
@@ -14,28 +14,25 @@ public class redshiftdbtest {
 
 		// Connection con =DriverManager.getConnection(url, user, password);
 
-		Connection con = DriverManager.getConnection(
-				"jdbc:redshift://redshift-qa.foxanalyticsgroup.com:5439/edh?ssl=true", "svc_selenium_poc",
-				"lti_Fox_@123");
-
+		String url = "jdbc:redshift://redshift-qa.foxanalyticsgroup.com:5439/edh?ssl=true";
+		String user = "svc_selenium_poc";
+		String pass = "lti_Fox_@123";
+		Connection con = DriverManager.getConnection(url, user, pass);
 
 		String sql = "select  program_group,  sum(cy_to_sold_dollars_booked) booked,  sum(cy_to_sold_dollars_working) working,"
 				+ "sum(cy_variance_sold_dollars_booked) activity from fox_ai_instrumentation.v_ft_cpw_sales_activity_fact "
-				+ "where activity_week_start = '2020-01-30' " 
-				+ "and program_group='WWE'"
-				 +"and business in ('Sports', 'Entertainment') group by 1";
+				+ "where activity_week_start = '2020-01-30' " + "and program_group='WWE'"
+				+ "and business in ('Sports', 'Entertainment') group by 1";
 
 		Statement s = con.createStatement();
 		ResultSet res = s.executeQuery(sql);
 
 		while (res.next()) {
 
-
-			System.out.println(res.getString("program_group") );
-			System.out.println(res.getString("booked") );
-			System.out.println(res.getString("working") );
-			System.out.println(res.getString("activity") );
-			
+			System.out.println(res.getString("program_group"));
+			System.out.println(res.getString("booked"));
+			System.out.println(res.getString("working"));
+			System.out.println(res.getString("activity"));
 
 		}
 
