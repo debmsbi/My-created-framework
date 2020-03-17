6f214fe55c0d2ee31518d00fd3a28744f785b42f
10647421 <10647421@INFPHSZA07218.lntinfotech.com>
2020-03-17 23:28:40 +0530

    updated

--- src/test/java/com/imf/qa/test/redshiftdbtest.java
+++ src/test/java/com/imf/qa/test/redshiftdbtest.java
@@ -14,10 +14,25 @@ public class redshiftdbtest extends Loginredshift {
 
 		Connection con = DriverManager.getConnection(url, user, pass);
 
-		String sql = "select  program_group,  sum(cy_to_sold_dollars_booked) booked,  sum(cy_to_sold_dollars_working) working,"
-				+ "sum(cy_variance_sold_dollars_booked) activity from fox_ai_instrumentation.v_ft_cpw_sales_activity_fact "
-				+ "where activity_week_start = '2020-01-30' " + "and program_group='WWE'"
-				+ "and business in ('Sports', 'Entertainment') group by 1";
+		String sql = "select activity_week_start," + "       count(*),"
+				+ "       sum(cy_to_sold_dollars_booked) as sold_dollars_booked,"
+				+ "       sum(cy_variance_sold_dollars_booked) as variance_sold_dollars_booked,"
+				+ "       sum(cy_variance_new_money_sold_dollars_booked),"
+				+ "       sum(cy_variance_orig_ratecard_rate_booked),"
+				+ "       sum(cy_variance_guaranteed_impressions_booked)," 
+				+ "       sum(cy_from_sold_dollars_working),"
+				+ "       sum(cy_to_sold_dollars_working) as sold_dollars_working, " 
+				+ "       sum(cy_variance_sold_dollars_working),"
+				+ "       sum(cy_variance_new_money_sold_dollars_working),"
+				+ "       sum(cy_variance_orig_ratecard_rate_working)," 
+				+ "       sum(py_to_sold_dollars_booked),  "
+				+ "       sum(py_variance_sold_dollars_booked),"
+				+ "       sum(py_variance_new_money_sold_dollars_booked),"
+				+ "       sum(py_variance_orig_ratecard_rate_booked)," 
+				+ "       sum(py_booked_total)  "
+				+ "from fox_ai_instrumentation.v_ft_cpw_sales_activity_fact " 
+				+ "where activity_week_start='2020-02-20'"
+				+ "group by 1" + "order by 1 desc;";
 
 		Statement s = con.createStatement();
 		ResultSet res = s.executeQuery(sql);
@@ -25,17 +40,14 @@ public class redshiftdbtest extends Loginredshift {
 		while (res.next()) {
 
 			System.out.println(
-					"Value for program_group column in tablename fox_ai_instrumentation.v_ft_cpw_sales_activity_fact  :  "
-							+ res.getString("program_group"));
+					"Value for sum(cy_to_sold_dollars_booked) column in tablename fox_ai_instrumentation.v_ft_cpw_sales_activity_fact  :  "
+							+ res.getString("sold_dollars_booked"));
 			System.out.println(
-					"Value for booked column in tablename fox_ai_instrumentation.v_ft_cpw_sales_activity_fact  :  "
-							+ res.getString("booked"));
+					"Value for sum(cy_variance_sold_dollars_booked) column in tablename fox_ai_instrumentation.v_ft_cpw_sales_activity_fact  :  "
+							+ res.getString("variance_sold_dollars_booked"));
 			System.out.println(
-					"Value for working column in tablename fox_ai_instrumentation.v_ft_cpw_sales_activity_fact  :  "
-							+ res.getString("working"));
-			System.out.println(
-					"Value for activity column in tablename fox_ai_instrumentation.v_ft_cpw_sales_activity_fact  :  "
-							+ res.getString("activity"));
+					"Value for sum(cy_to_sold_dollars_working)  column in tablename fox_ai_instrumentation.v_ft_cpw_sales_activity_fact  :  "
+							+ res.getString("sold_dollars_working"));
 
 		}
 
